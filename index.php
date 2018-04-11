3c7d50360efcf54fa827e45cd171d13948a6cf1a
Marcos Paulo <marcosmp1516@gmail.com>
2018-04-11 20:27:53 -0300

    criamos a class usuario

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 require_once("confing.php");
-$sql = new Sql();
+/*$sql = new Sql();
 $usuario = $sql->select("SELECT * FROM tb_usuario");
 
-echo json_encode($usuario);
+echo json_encode($usuario);*/
+
+$marcos = new Usuario();
+
+$marcos->loadById(10);
+
+echo $marcos;
 
  ?>
